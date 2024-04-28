c54a90878b649b19d35b99248bfbebeba6f02e79
Mangesh Ahirrao <139425758+mangesh925@users.noreply.github.com>
2024-04-28 02:25:25 +0000

    Updates

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,6 @@
+const accountId = 12233443
+let accountEmail = "mangesh@mangesh.com"
+var accountPassword = "12345"
+accountCity = "Bhopal"
+// accountId = 2
+console.table([accountId, accountEmail, accountPassword, accountCity])
